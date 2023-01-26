d3fcc2c9bb45db18a6e644728c6003988fc68f9d
Faheemuddin Naseem <fnaseem010@gmail.com>
2023-01-27 05:22:00 +0530

    done whatgpt3 container

--- src/containers/WHATGPT3/WHATGPT3.jsx
+++ src/containers/WHATGPT3/WHATGPT3.jsx
@@ -2,7 +2,44 @@ import React from 'react'
 import './whatgpt3.css'
 const WHATGPT3 = () => {
   return (
-    <div>WHATGPT3</div>
+    <div className='gpt3__whatgpt3'>
+      <div className='gpt3__whatgpt3-upper'>
+<div className='gpt3__whatgpt3-upper_header'>
+<div className='gpt3__whatgpt3-line_bar'></div>
+<h2>What is GPT-3</h2>
+</div>
+<div className='gpt3__whatgpt3-upper_para'>
+  <p>We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.</p>
+
+</div>
+      </div>
+      <div className='gpt3__whatgpt3-middle'>
+        <div className='gpt3__whatgpt3-middle_header'>
+<h1>The possibilities are beyond your imagination</h1>
+        </div>
+        <div className='gpt3__whatgpt3-middle_para'>
+<p>Explore The Library</p>
+        </div>
+      </div>
+      <div className='gpt3__whatgpt3-footer'>
+      
+        <div className='gpt3__whatgpt3-footer_block'>
+        <div className='gpt3__whatgpt3-line_bar'></div>
+<h2>Chatbots</h2>
+<p>We so opinion friends me message as delight. Whole front do of plate heard oh ought.</p>
+        </div>
+        <div className='gpt3__whatgpt3-footer_block'>
+        <div className='gpt3__whatgpt3-line_bar'></div>
+<h2>Knowledgebase</h2>
+<p>At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments.</p>
+        </div>
+        <div className='gpt3__whatgpt3-footer_block'>
+        <div className='gpt3__whatgpt3-line_bar'></div>
+<h2>Education</h2>
+<p>At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments.</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
